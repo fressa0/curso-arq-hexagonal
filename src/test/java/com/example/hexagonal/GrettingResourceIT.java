2b4443e7d3a257e09ff4af006917baf92f333738
Francesc Escobar <francesces@ext.inditex.com>
2024-07-01 13:25:18 +0200

    initial commit

---
+++ src/test/java/com/example/hexagonal/GrettingResourceIT.java
@@ -0,0 +1,8 @@
+package com.example.hexagonal;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GrettingResourceIT extends GrettingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ src/test/java/com/example/hexagonal/ProductResourceTest.java
@@ -0,0 +1,20 @@
+package com.example.hexagonal;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class ProductResourceTest {
+    @Test
+    void testResourceEndpoint() {
+        given()
+          .when().get("/products")
+          .then()
+             .statusCode(200)
+             .body(is("Listado de productos 01-07-2024"));
+    }
+
+}
